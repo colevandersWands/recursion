93bca136d31e49d7d3bc99e682e438e2f21b14f5
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2019-01-02 18:27:17 +0100

    bettered

--- 0-recursion/function-types.js
+++ 0-recursion/function-types.js
@@ -1,14 +1,5 @@
 { console.log('# recursion with different function types ')
 
-console.log('(test cases)');
-  const recurse_cases = [
-      {name: '-1', args: [-1], expected: undefined},
-      {name: '0', args: [0], expected: undefined},
-      {name: '1', args: [1], expected: undefined},
-      {name: '2', args: [2], expected: undefined},
-      {name: '3', args: [3], expected: undefined},
-      {name: '4', args: [4], expected: undefined},
-    ]
 
 { console.log('## standard function');
 
@@ -20,9 +11,9 @@ console.log('(test cases)');
       return {base: false, a, reclog};
     }
   };
-  console.log('recurse(test_cases)');
   console.log('recurse: ', recurse)
-  log_reports(recurse, recurse_cases);
+  console.log(recurse(1));
+ 
 
   console.log('*breaks if reassigned variables*')
   const other_var = recurse;
@@ -30,8 +21,7 @@ console.log('(test cases)');
   console.log('recurse: ', recurse);
   console.log('other_var: ', other_var);
   try {
-    console.log('other_var(test_cases)'); /// go through and fix these.  change to camelcase, line up the right names with the right calls
-    log_reports(other_var, recurse_cases);
+    console.log(recurse(1));
   } catch(err) {
     console.log(err);
   }
@@ -47,9 +37,9 @@ console.log('(test cases)');
       return {base: false, a, reclog};
     }
   };
-  console.log('recurse(test_cases)');
-  console.log('recurse: ', recurse)
-  log_reports(recurse, recurse_cases);
+  console.log('recurse: ', recurse);
+  console.log(recurse(1));
+ 
 
   console.log('*cant reassign to new variables*')
   const other_var = recurse;
@@ -57,8 +47,7 @@ console.log('(test cases)');
   console.log('recurse: ', recurse);
   console.log('other_var: ', other_var);
   try {
-    console.log('other_var(test_cases)');
-    log_reports(other_var, recurse_cases);
+    console.log(recurse(1));
   } catch(err) {
     console.log(err);
   }
@@ -74,14 +63,13 @@ console.log('(test cases)');
       return {base: false, a, reclog};
     }
   };
-  console.log('recurse(test_cases)');
   console.log('public_name: ', public_name)
-  log_reports(public_name, recurse_cases);
+  console.log(public_name(1));
+ 
 
   console.log('*cant call inner name from outside*')
   try {
-    console.log('recurse(test_cases)');
-    log_reports(private_name, recurse_cases);
+    console.log(private_name(1));
   } catch(err) {
     console.log(err);
   }
@@ -92,18 +80,18 @@ console.log('(test cases)');
   console.log('public_name: ', public_name)
   console.log('other_var: ', other_var);
   try {
-    log_reports(other_var, recurse_cases);
+    console.log(other_var(1));
   } catch(err) {
     console.log(err);
   }
 
-  console.log('*the inner function name is protected*')
+  console.log('*the inner function name is preserved inside the original function*')
   const private_name = (a) => a;
   console.log('private_name: ', private_name);
-
   console.log('other_var: ', other_var);
+
   try {
-    log_reports(other_var, recurse_cases);
+    console.log(other_var(1));  
   } catch(err) {
     console.log(err);
   }
@@ -111,14 +99,4 @@ console.log('(test cases)');
 
 
 
-
-// ------ test utils ------
-
-function log_reports(_target, _cases) {
-  const report = {}
-  for (let t_case of _cases) {
-     report[t_case.name] = _target(...t_case.args) 
-  };
-  console.log(report)
-};
 }
